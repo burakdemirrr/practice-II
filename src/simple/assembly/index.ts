3ca2e90fcfdd668aea74e76e34c67717a5399866
Burak <46084021+burakdemirrr@users.noreply.github.com>
2022-04-16 17:42:44 +0300

    Update index.ts

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,6 +1,6 @@
 import { storage, Context } from "near-sdk-as"
 
-// return the string 'hello world'
+
 export function helloWorld(): string {
   const predecessor =Context.predecessor
   return 'hello '+predecessor
